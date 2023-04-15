4c44757c477155891fa61ad91f6f1b9b7346c4e3
saipavan1925 <bsaipavan98@gmail.com>
2023-04-15 15:52:06 -0400

    Flask updated with DB

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import configparser
-from flask import Flask, redirect, render_template, request, url_for
+from multiprocessing import connection
+from flask import Flask, Response, redirect, render_template, request, url_for
+
 import sqlite3
 app = Flask(__name__)
 
@@ -46,21 +48,104 @@ def paneer_tikka():
 def signup():
     return render_template('signup.html')
 #------------add_item--
-@app.route('/additem')
-def add_item():
-     return render_template('add_item.html')
+
 #-------sql-----
 
-#--------@app.route('/add_item', methods=['GET', 'POST'])
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+print("Opened database successfully")
+
+"""conn.execute('''CREATE TABLE items
+             (id INTEGER PRIMARY KEY,
+             category TEXT,
+             item_name TEXT,
+             price REAL,
+             item_image BLOB);''')
+print("Table created successfully")"""
+
+conn.close()
+
+@app.route('/enternew')
+def new_item():
+   return render_template('item.html')
+
+@app.route('/add_item', methods=['POST'])
 def add_item():
-	if request.method == 'POST':
-		name = request.form['name']
-		price = request.form['price']
-		conn = sqlite3.connect('items.db')
-		c = conn.cursor()
-		c.execute('INSERT INTO items (name, price) VALUES (?, ?)', (name, price))
-		conn.commit()
-		conn.close()
-		return 'Item added successfully!'
-	else:
-		return render_template('add_item.html')
+    if request.method == 'POST':
+        try:
+            category = request.form['category']
+            item_name = request.form['item_name']
+            price = request.form['price']
+            item_image = request.files['item_image'].read()
+            
+            with sqlite3.connect('database.db') as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO items (category, item_name, price, item_image) VALUES (?, ?, ?, ?)", 
+                            (category, item_name, price, item_image))
+                
+                con.commit()
+                msg = "Record successfully added"
+        except:
+            con.rollback()
+            msg = "Error in insert operation"
+        finally:
+            return render_template("result.html", msg=msg)
+
+
+   
+@app.route('/view_items_by_category', methods=['GET'])
+def view_items_by_category():
+    category = request.args.get('category')
+    with sqlite3.connect('database.db') as con:
+        cur = con.cursor()
+        cur.execute("SELECT item_name, price, item_image FROM items WHERE category = ?", (category,))
+        rows = cur.fetchall()
+    return render_template('view_items_by_category.html', rows=rows)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------to delete the db--------------   
+"""import os
+import sqlite3
+
+# Close any open connections to the database
+conn = sqlite3.connect('database.db')
+conn.close()
+
+# Drop the items table
+conn = sqlite3.connect('database.db')
+conn.execute("DROP TABLE items")
+conn.close()
+
+# Delete the database file
+os.remove('database.db')"""
