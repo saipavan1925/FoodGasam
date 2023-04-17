27e11207d0d4854b78f35896534e322056e8e534
saipavan1925 <bsaipavan98@gmail.com>
2023-04-17 12:01:43 -0400

    Updated enternew & Menu pages with cart added

--- app.py
+++ app.py
@@ -27,7 +27,13 @@ def aboutus():
 
 @app.route("/Menu/")
 def Menu():
-    return render_template('Menu.html')
+     with sqlite3.connect('database.db') as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("SELECT * FROM items")
+        rows = cur.fetchall()
+        return render_template('Menu.html', rows=rows)
+    
 
 @app.route('/signin')
 def signin():
@@ -58,13 +64,13 @@ import sqlite3
 
 conn = sqlite3.connect('database.db')
 print("Opened database successfully")
-conn.execute('DROP TABLE IF EXISTS items')
+"""conn.execute('DROP TABLE IF EXISTS items')
 conn.commit()
 conn.execute('''CREATE TABLE items 
              (id INTEGER PRIMARY KEY,
              category TEXT,
              item_name TEXT,
-             price REAL);''')
+             price REAL);''')"""
 print("Table created successfully")
 
 conn.close()
@@ -72,6 +78,8 @@ conn.close()
 @app.route('/enternew')
 def new_item():
    return render_template('item.html')
+
+
 
 @app.route('/add_item', methods=['POST'])
 def add_item():
@@ -94,24 +102,73 @@ def add_item():
             msg = "Error in insert operation"
         finally:
             return render_template("result.html", msg=msg)
+        
 
 
-   
-@app.route('/view_items_by_category', methods=['GET'])
+
+
+
+
+""" Well it worked but implemented the same function in menu.html @app.route('/view_items')
+def view_items():
+    with sqlite3.connect('database.db') as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("SELECT * FROM items")
+        rows = cur.fetchall()
+    return render_template('Menu.html', rows=rows)"""
+
+
+
+
+
+
+
+
+
+"""@app.route('/view_items_by_category', methods=['GET', 'POST'])
+def view_items_by_category():
+    if request.method == 'POST':
+        selected_category = request.form.get('category')
+        with sqlite3.connect('database.db') as con:
+            con.row_factory = sqlite3.Row
+            cur = con.cursor()
+            cur.execute("SELECT item_name, price FROM items WHERE category = ?", (selected_category,))
+            rows = cur.fetchall()
+            return render_template('view_items_by_category.html', rows=rows, selected_category=selected_category)
+    else:
+        with sqlite3.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute("SELECT DISTINCT category FROM items")
+            categories = cur.fetchall()
+        return render_template('select_category.html', categories=categories)"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""@app.route('/view_items_by_category', methods=['GET'])
 def view_items_by_category():
     ##category = request.args.get('category')
    # with sqlite3.connect('database.db') as con:
     con = sqlite3.connect('database.db')
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute("SELECT item_name, price,item_image FROM items")
-    image_data = cur.fetchone()[2]
-    image = Image.open(io.BytesIO(image_data)) 
+    cur.execute("SELECT item_name, priceFROM items") 
   
 
         ##cur.execute("SELECT item_name, price FROM items WHERE category = ?", (category,))
     rows = cur.fetchall();
-    return render_template('view_items_by_category.html', rows = rows)
+    return render_template('view_items_by_category.html', rows = rows)"""
 
 
 """@app.route('/view_items_by_category', methods=['GET'])
@@ -157,7 +214,7 @@ def view_items_by_category():
 
 
 #------to delete the db--------------   
-import os
+"""import os
 import sqlite3
 
 # Close any open connections to the database
@@ -170,4 +227,4 @@ conn.execute("DROP TABLE items")
 conn.close()
 
 # Delete the database file
-os.remove('database.db')
+os.remove('database.db')"""
